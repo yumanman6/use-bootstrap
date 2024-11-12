b4787babb58b8bd80804a23eba0156939cafcd6b
yumanman6 <1677543467@qq.com>
2024-11-12 14:41:35 +0800

    Initial commit

---
+++ pages/fixture/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Components',
+  description: 'Components Examples for testing.'
+})
+</script>
+
+<template>
+  <ComponentsCatalog />
+</template>
